d83aaaf4b943116d4458d98992ac04d2f63468ca
graja22 <48359331+graja22@users.noreply.github.com>
2019-05-21 12:11:43 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,8 @@
+a=str(input());
+if ((a>='a') & (a<='z')or (a>='A') & (a<='Z')): 
+    if a=='a'or a=='e'or a=='i'or a=='o'or a=='u'or a=='A'or a=='E'or a=='I'or a=='O'or a=='U':
+        print("vowel")
+    else:
+        print("consonant")
+else:
+    print("invalid")
