02698b80234ad4aa16868bdbed6300da7be19e94
graja22 <48359331+graja22@users.noreply.github.com>
2019-07-09 12:43:16 +0530

    Update nk.py

--- nk.py
+++ nk.py
@@ -1,8 +1,8 @@
-n = int(input())
-k=int(input())
+c = int(input())
+d=int(input())
 arr = []
-for i in range(n):
-    c= int(input())
-    arr.append(c) 
-d=k*(k+1)/2
-print(int(d))
+for i in range(c):
+    e= int(input())
+    arr.append(e) 
+f=d*(d+1)/2
+print(int(f))
