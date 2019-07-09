d2a5b3be8c6e0204ba75c3b902cbf642954e8ec2
graja22 <48359331+graja22@users.noreply.github.com>
2019-07-09 10:52:38 +0530

    Update alp.py

--- alp.py
+++ alp.py
@@ -1,6 +1,6 @@
-a=str(input());
+c=str(input());
 alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-if a in alpha:
+if c in alpha:
     print("Alphabet")
 else:
     print("No")
