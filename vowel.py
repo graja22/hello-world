401bf9a500b6a108ada3eb41ba0035c8233854f6
graja22 <48359331+graja22@users.noreply.github.com>
2019-07-09 10:48:05 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,8 +1,8 @@
-a=str(input());
-if ((a>='a') & (a<='z')or (a>='A') & (a<='Z')): 
-    if a=='a'or a=='e'or a=='i'or a=='o'or a=='u'or a=='A'or a=='E'or a=='I'or a=='O'or a=='U':
-        print("vowel")
+c=str(input());
+if ((c>='a') & (c<='z')or (c>='A') & (c<='Z')): 
+    if c=='a'or c=='e'or c=='i'or c=='o'or c=='u'or c=='A'or c=='E'or c=='I'or c=='O'or c=='U':
+        print("Vowel")
     else:
-        print("consonant")
+        print("Consonant")
 else:
     print("invalid")
