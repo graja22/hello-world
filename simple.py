e11f5d96f8ee943d5d2e51a3152157cdc50b16de
graja22 <48359331+graja22@users.noreply.github.com>
2019-07-09 10:35:04 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,9 +1,9 @@
-a=int(input());
-if a > 0:
+c=int(input());
+if c > 0:
     print("Positive")
-elif a < 0:
+elif c < 0:
     print("Negative")
-elif a==0:
+elif c==0:
     print("Zero")
 else:
     print("Invaid input")
