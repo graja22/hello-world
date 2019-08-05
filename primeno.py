fa200731e4324086766a2b9fc947366cd2584793
graja22 <48359331+graja22@users.noreply.github.com>
2019-08-05 16:59:40 +0530

    Add files via upload

---
+++ primeno.py
@@ -0,0 +1,12 @@
+a = int(input())  
+if a > 1:  
+   for i in range(2,a):  
+       if (a % i) == 0:  
+           print("no ")  
+           
+           break  
+   else:  
+       print("yes")  
+         
+else:  
+   print("no")  
